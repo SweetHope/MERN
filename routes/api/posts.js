2f8c09121c7faeb7858bd05314b8998427109638
查甘望 <zhaganwang@timeface.cn>
2018-07-17 01:50:14 +0800

    增加profile删除接口、新增post数据模型

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
+// Post Model
+const Post = require("../../modeles/Post");
+
+// Validation
+const validatePostInput = require("../../validatation/post");
 // @route GET api/posts/test
 // @desc Tests post route
 // @access Public
@@ -8,4 +15,34 @@ router.get("/test", (req, res) => {
   res.json({ msg: "Posts Works" });
 });
 
+// @route POST api/posts
+// @desc Create post
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If andy errors, send 400 with errors object
+      res.send(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    newPost
+      .save()
+      .then(post => {
+        res.json(post);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+);
 module.exports = router;
